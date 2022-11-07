e8ed8589b63d5426cd6d63ab12be0d38732d4233
Enobong Bassey <34889029+Enobong-Bassey@users.noreply.github.com>
2022-11-07 02:33:01 -0500

    Add files via upload

---
+++ angular.js
@@ -0,0 +1,21 @@
+
+angular.module('coolApp' , []).controller('coolCtrl', function($scope){
+    $scope.cheqCredit = [
+        'Oct. 23, 2021      NSF Return Fee          $ 450.00',
+        {/* <tr> <td>Sep. 03, 2021</td> <td>CANADA PRO</td> <td>$ 2100.00</td> </tr>,
+        <tr> <td>Sep. 03, 2021</td> <td>FIDELITY MSP</td> <td>$ 3452.19</td> </tr>,
+        <tr> <td>Mary</td> <td>Moe</td> <td>mary@example.com</td> </tr>,
+        <tr> <td>July</td> <td>Dooley</td> <td>july@example.com</td> </tr> */}
+    ];
+});
+
+/* angular.module('coolApp' , []).controller('coolCtrl', function($scope){
+    $scope.cheqDebit = [
+        '<tr> <td>Oct. 23, 2021</td> <td>NSF Return Fee</td> <td>$ 450.00</td> </tr>',
+        '<tr> <td>Sep. 03, 2021</td> <td>CANADA PRO</td> <td>$ 2100.00</td> </tr>',
+        '<tr> <td>Sep. 03, 2021</td> <td>FIDELITY MSP</td> <td>$ 3452.19</td> </tr>',
+        '<tr> <td>Mary</td> <td>Moe</td> <td>mary@example.com</td> </tr>',
+        '<tr> <td>July</td> <td>Dooley</td> <td>july@example.com</td> </tr>'
+    ];
+});
+ */
--- app.js
+++ app.js
@@ -6,11 +6,30 @@ $(document).ready(function(){
     $('#dropmsg_subject_lbl').hide();
     $('#dropmsg_message_lbl').hide();
 
+    $("#savingsSectn").slideUp();
+    $("#creditcardSectn").slideUp();
+    accountTypeHeader.acctTypeTransactLabel = "CHEQUING ACCOUNT";
+    $("#chequingSectn").slideDown();
+
     $('#otherOffices').load("blank.txt");
 
-    $('#personalListBtns').load("blank.txt");
-    $('#subAppmntTypeListBtns').load("blank.txt");
-    $('#appointmentStep4').load("blank.txt");
+    // handling booking an appointment section
+    $('#personalStep1').slideUp();
+    $('#businessStep1').slideUp();
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideUp();
+
+    // $('#personalListBtns').load("blank.txt");
+    // $('#subAppmntTypeListBtns').load("blank.txt");
+    // $('#appointmentStep4').load("blank.txt");
 
 });
 
@@ -22,7 +41,7 @@ var accountTypeHeader = new Vue({
 
 // functions called by chequingBtn, savingsBtn and creditcardsBtn buttons
 // For chequing account
-function changeToChequing() {
+/* function changeToChequing() {
     accountTypeHeader.acctTypeTransactLabel = "CHEQUING ACCOUNT";
 }
 
@@ -35,9 +54,33 @@ function changeToSavings() {
 function changeToCreditCards() {
     accountTypeHeader.acctTypeTransactLabel = "CREDIT CARD";
 }
+ */
+
+$("#chequingBtn").click(function(){
+    $("#savingsSectn").slideUp();
+    $("#creditcardSectn").slideUp();
+    accountTypeHeader.acctTypeTransactLabel = "CHEQUING ACCOUNT";
+    $("#chequingSectn").slideDown();   
+});
+
+$("#savingsBtn").click(function(){
+    $("#creditcardSectn").slideUp();
+    $("#chequingSectn").slideUp();
+    accountTypeHeader.acctTypeTransactLabel = "SAVINGS ACCOUNT";
+    $("#savingsSectn").slideDown();
+});
+
+$("#creditcardsBtn").click(function(){
+    $("#savingsSectn").slideUp();
+    $("#chequingSectn").slideUp();
+    accountTypeHeader.acctTypeTransactLabel = "CREDIT CARD";
+    $("#creditcardSectn").slideDown();
+});
 
 // Ajax code to filter
 // Ajax example
+
+// Chequing account section
 $('#creditSearchChequing').on("keyup", function(){
     var value = $(this).val().toLowerCase();
     $('#listCheqC tr').filter(function(){
@@ -48,6 +91,36 @@ $('#creditSearchChequing').on("keyup", function(){
 $('#debitSearchChequing').on("keyup", function(){
     var value = $(this).val().toLowerCase();
     $('#listCheqD tr').filter(function(){
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)    // index (0 - 10)
+    });
+});
+
+// Savings account section
+$('#creditSearchSaving').on("keyup", function(){
+    var value = $(this).val().toLowerCase();
+    $('#listSavingC tr').filter(function(){
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)    // index (0 - 10)
+    });
+});
+
+$('#debitSearchSaving').on("keyup", function(){
+    var value = $(this).val().toLowerCase();
+    $('#listSavingD tr').filter(function(){
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)    // index (0 - 10)
+    });
+});
+
+// Credit Card section
+$('#creditSearchCreditCard').on("keyup", function(){
+    var value = $(this).val().toLowerCase();
+    $('#listCreditCardC tr').filter(function(){
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)    // index (0 - 10)
+    });
+});
+
+$('#debitSearchCreditCard').on("keyup", function(){
+    var value = $(this).val().toLowerCase();
+    $('#listCreditCardD tr').filter(function(){
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)    // index (0 - 10)
     });
 });
@@ -77,70 +150,224 @@ $("#showLocations").click(function(){
     }else{
         $('#otherOffices').load("blank.txt");
         $("#showLocations").html("show more");
-        $('#transactionsH').load("chequing.txt");
     }
 });
 
-// script to load text files
+
+// handling contact booking appointment button clicks
 $('#personalBtn').click(function(){
-    $('#personalListBtns').load("nextOnPersonal.txt");
-    $('#personalBtn').css()
+    $('#businessStep1').slideUp();    
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideUp();
+    $('#personalStep1').slideDown();
 });
 
-// Showing transaction history
-$('#chequingBtn').click(function(){
-    $('#transactionsH').load("chequing.txt");
-});
-
-$('#savingsBtn').click(function(){
-    $('#transactionsH').load("savings.txt");
-});
-
-$('#creditcardBtn').click(function(){
-    $('#transactionsH').load("creditcard.txt");
+$('#businessBtn').click(function(){
+    $('#personalStep1').slideUp();    
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideUp();
+    $('#businessStep1').slideDown();
 });
 
 
 $('#bankingBtn').click(function(){
-    $('#subAppmntTypeListBtns').load("nextOnBanking.txt");
-});
-
-$('#showContact').click(function(){
-    $('#contactInfo').load("info.txt");
-});
-
-$('#openChequing').click(function(){
-    $('#appointmentStep4').load("Step4Banking.txt");
-});
-
-$('#openSavings').click(function(){
-    $('#appointmentStep4').load("Step4Banking.txt");
-});
-
-$('#openUSdollar').click(function(){
-    $('#appointmentStep4').load("Step4Banking.txt");
-});
-
-$('#applyOverdraftP').click(function(){
-    $('#appointmentStep4').load("Step4Banking.txt");
+    $('#businessStep1').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideDown();
+    $('#pbankingStep2').slideDown();
 });
 
 $('#investingBtn').click(function(){
-    $('#subAppmntTypeListBtns').load("nextOnInvesting.txt");
+    $('#businessStep1').slideUp();
+    $('#pbankingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideDown();
+    $('#pinvestingStep2').slideDown();
 });
 
-$('#startsaving').click(function(){
-    $('#appointmentStep4').load("Step4Banking.txt");
+$('#homeFinancingBtn').click(function(){
+    $('#businessStep1').slideUp();
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideDown();
+    $('#phomefinancingStep2').slideDown();
 });
 
-$('#retirement').click(function(){
-    $('#appointmentStep4').load("Step4Banking.txt");
+$('#borrowingBtn').click(function(){
+    $('#businessStep1').slideUp();
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideDown();
+    $('#pborrowingStep2').slideDown();
 });
 
-$('#mutualfunds').click(function(){
-    $('#appointmentStep4').load("Step4Banking.txt");
+$('#creditcardBtn').click(function(){
+    $('#businessStep1').slideUp();
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideDown();
+    $('#pcreditcardsStep2').slideDown();
 });
 
-$('#childeduc').click(function(){
-    $('#appointmentStep4').load("Step4Banking.txt");
+$('#bankingBtnBiz').click(function(){
+    $('#personalStep1').slideUp();    
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideDown();
+    $('#bbankingStep2').slideDown();
 });
+
+$('#investingBtnBiz').click(function(){
+    $('#personalStep1').slideUp();    
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideDown();
+    $('#binvestingStep2').slideDown();
+});
+
+$('#borrowingBtnBiz').click(function(){
+    $('#personalStep1').slideUp();    
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bcreditcardsStep2').slideUp();
+    $('#appointmentStep4').slideDown();
+    $('#bborrowingStep2').slideDown();
+});
+
+$('#creditcardBtnBiz').click(function(){
+    $('#personalStep1').slideUp();    
+    $('#pbankingStep2').slideUp();
+    $('#pinvestingStep2').slideUp();
+    $('#phomefinancingStep2').slideUp();
+    $('#pborrowingStep2').slideUp();
+    $('#pcreditcardsStep2').slideUp();
+    $('#bbankingStep2').slideUp();
+    $('#binvestingStep2').slideUp();
+    $('#bborrowingStep2').slideUp();
+    $('#appointmentStep4').slideDown();
+    $('#bcreditcardsStep2').slideDown();
+});
+
+// script to load text files
+// $('#personalBtn').click(function(){
+//     $('#personalListBtns').load("nextOnPersonal.txt");
+//     $('#personalBtn').css()
+// });
+
+// Showing transaction history
+// $('#chequingBtn').click(function(){
+//     $('#transactionsH').load("chequing.txt");
+// });
+
+// $('#savingsBtn').click(function(){
+//     $('#transactionsH').load("savings.txt");
+// });
+
+// $('#creditcardBtn').click(function(){
+//     $('#transactionsH').load("creditcard.txt");
+// });
+
+
+// $('#showContact').click(function(){
+//     $('#contactInfo').load("info.txt");
+// });
+
+// $('#openChequing').click(function(){
+//     $('#appointmentStep4').load("Step4Banking.txt");
+// });
+
+// $('#openSavings').click(function(){
+//     $('#appointmentStep4').load("Step4Banking.txt");
+// });
+
+// $('#openUSdollar').click(function(){
+//     $('#appointmentStep4').load("Step4Banking.txt");
+// });
+
+// $('#applyOverdraftP').click(function(){
+//     $('#appointmentStep4').load("Step4Banking.txt");
+// });
+
+// $('#investingBtn').click(function(){
+//     $('#subAppmntTypeListBtns').load("nextOnInvesting.txt");
+// });
+
+// $('#startsaving').click(function(){
+//     $('#appointmentStep4').load("Step4Banking.txt");
+// });
+
+// $('#retirement').click(function(){
+//     $('#appointmentStep4').load("Step4Banking.txt");
+// });
+
+// $('#mutualfunds').click(function(){
+//     $('#appointmentStep4').load("Step4Banking.txt");
+// });
+
+// $('#childeduc').click(function(){
+//     $('#appointmentStep4').load("Step4Banking.txt");
+// });
